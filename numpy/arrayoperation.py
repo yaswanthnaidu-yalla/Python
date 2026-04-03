646022d3c21debfaaf57f1e145ecbef9e4f3324f
yaswanthnaidu-yalla <yaswanthnaiduyalla@gmail.com>
2026-04-03 22:44:41 +0530

    intro to numpy, comfortable using venv

---
+++ numpy/arrayoperation.py
@@ -0,0 +1,23 @@
+import numpy as np
+a = np.array([2,3,4])
+print(a)
+#dtype gives the type of elements present in the array.
+#type can be specified while creation
+c = np.array([[1,2],[3,4]], dtype=complex)
+print(c)
+#creating an array of zeroes and ones
+d = np.zeros((3,3))
+e= np.ones((4,4))
+print(d)
+print(e)
+
+#using arange to create arrays
+b = np.arange(1,10,2)
+print(b)
+
+#using linspace
+
+print(np.linspace(0,7,20))
+print(np.sin(a))
+
+
